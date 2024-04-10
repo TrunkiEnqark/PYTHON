a5cbb49ead4cab55c5507e42b102ce0bb362d303
Trunki Enqark <trunki.enqark@gmail.com>
2024-04-10 22:45:43 +0700

    .

--- VNOI/KSTRPOW.py
+++ VNOI/KSTRPOW.py
@@ -2,17 +2,8 @@ string_1 = input()
 string_2 = input()
 times = int(input())
 
-i = 0
-calc_times = 0
-while i + len(string_1) - 1 < len(string_2):
-    print(string_2[i:i + len(string_1)])
-    if string_2[i:i + len(string_1)] != string_1:
-        print("NO");
-        exit()
-    i += len(string_1)
-    calc_times += 1
-    
-if calc_times == times:
+string_1 *= times
+if string_1 == string_2:
     print("YES")
 else:
     print("NO")
